41cfa0b1039fd44c15c2a56a675ed5d4da596cab
Danny <dukeydk@gmail.com>
2017-12-07 20:31:34 -0500

    added some colors and started search

--- app/src/main/java/com/peter/daniel/photosapp/searchActivity.java
+++ app/src/main/java/com/peter/daniel/photosapp/searchActivity.java
@@ -6,16 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
-public class ViewAlbum extends AppCompatActivity {
+public class searchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_album);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_search);
 
     }
-
 }
